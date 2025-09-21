60bd7ec7552f5023c86d0200fabf8a7743944996
Hiddev666 <officehiddev@gmail.com>
2025-09-21 16:23:49 +0700

    dev: add master data migration file

---
+++ app/Database/Migrations/2025-09-21-061210_CreateMasterLocation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateMasterLocation extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
